34f0e01cf69ac37272bb5a8b265ff5c6447f76ae
ErayAlish <111420915+ErayAlish@users.noreply.github.com>
2023-02-14 00:14:28 +0100

    Uptade 1.05.09

--- test/Program.cs
+++ test/Program.cs
@@ -11,10 +11,14 @@ class Program
     {
         bool flag;
         bool flag2;
-        int menu = 0;
+        bool t = true; 
         double bet = 0;
         string betOption;
+       
         int timeLimit = 30; // Zeitlimit von 30 Sekunden vor dem Einsetzen
+        int bet2 = 0;
+        
+        int menu = 0;
         Random random = new Random();
 
         Console.WriteLine("Welcome");
@@ -55,58 +59,145 @@ class Program
             }
             if (menu == 2)
             {
-               
+
                 do
                 {
-                        Console.Clear();
-                        Console.WriteLine("How much would you like to bet?");
-                        Console.Write("bet > ");
-                        flag2 = (Double.TryParse(Console.ReadLine(), out bet));
+                    Console.Clear();
+                    Console.WriteLine("How much would you like to bet?");
+                    Console.Write("bet > ");
+                    flag2 = (Double.TryParse(Console.ReadLine(), out bet));
 
 
 
-                        if (flag2 == true && bet < 1)
+                    if (flag2 == true && bet < 1)
+                    {
+
+                        Console.WriteLine("Wrong input. No negative numbers or zero.");
+                        Console.WriteLine("Press to bet again");
+                        Console.ReadKey();
+                      
+                    }
+                    else if (flag2 == false)
+                    {
+
+                        Console.WriteLine("Wrong input");
+                        Console.WriteLine("Press to bet again");
+                        Console.ReadKey();
+                       
+                    }
+
+                }while (true);
+
+                    var timer = System.Diagnostics.Stopwatch.StartNew();
+                    int randomNumber = random.Next(0, 36);
+                if (flag2 == true)
+                {
+                    do
+                    {
+                        do
                         {
-
-                            Console.WriteLine("Wrong input. No negative numbers or zero.");
-                            Console.WriteLine("Press to bet again");
-                            Console.ReadKey();
-
-                        }
-                        else if (flag2 == false)
-                        {
-
-                            Console.WriteLine("Wrong input");
-                            Console.WriteLine("Press to bet again");
-                            Console.ReadKey();
-
-                        }
-                        var timer = System.Diagnostics.Stopwatch.StartNew();
-                        while (timer.ElapsedMilliseconds / 1000 < timeLimit)
-                        {
-                            Console.Write("Ihre Wette: ");
+                            Console.Clear();
+                            Console.WriteLine("Auf was möchten Sie wetten?");
+                            Console.WriteLine("Black; Red; Odd; Straight; Number");
+                            Console.Write("You bett: ");
                             betOption = Console.ReadLine().ToLower();
-                            if (betOption == "schwarz" || betOption == "rot" || betOption == "gerade" || betOption == "ungerade")
+                            randomNumber = random.Next(0, 36);
+                            if (betOption == "Number")
                             {
-                                break;
+                                Console.WriteLine("bett on 1-36");
+                                flag = (int.TryParse(Console.ReadLine(), out bet2));
+                            }
+                            if (flag == true && bet2 <= 0)
+                            {
+
+                                Console.WriteLine("Wrong input");
+                                Thread.Sleep(1000);
+                                Console.Clear();
+
+                            }
+                            else if (flag == true && bet2 > 36)
+                            {
+
+                                Console.WriteLine("Wrong input");
+                                Thread.Sleep(1000);
+                                Console.Clear();
+
+                            }
+                            else if (flag == false)
+                            {
+
+                                Console.WriteLine("Wrong input");
+                                Thread.Sleep(1000);
+                                Console.Clear();
+
+                            }
+                        } while (true);
+
+                        if (betOption == "Black")
+                        {
+                            if (randomNumber % 2 == 0)
+                            {
+                                Console.WriteLine("Sie haben gewonnen!");
+                                Console.WriteLine("Gewinn: " + bet * 2);
                             }
                             else
                             {
-                                Console.WriteLine("Ungültige Wette, bitte erneut versuchen");
+                                Console.WriteLine("Sie haben verloren!");
                             }
-                            if (timer.ElapsedMilliseconds / 1000 >= timeLimit)
+                        }
+                        else if (betOption == "Red")
+                        {
+                            if (randomNumber % 2 != 0)
                             {
-                                Console.WriteLine("Zeitlimit überschritten, Sie haben verloren.");
-                                continue;
+                                Console.WriteLine("Sie haben gewonnen!");
+                                Console.WriteLine("Gewinn: " + bet * 2);
                             }
-
-
-
-
+                            else
+                            {
+                                Console.WriteLine("Sie haben verloren!");
+                            }
+                        }
+                        else if (betOption == "Odd")
+                        {
+                            if (randomNumber % 2 != 0)
+                            {
+                                Console.WriteLine("Sie haben gewonnen!");
+                                Console.WriteLine("Gewinn: " + bet * 2);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Sie haben verloren!");
+                            }
+                        }
+                        else if (betOption == "Straight")
+                        {
+                            if (betOption = randomNumber)
+                            {
+                                Console.WriteLine("Sie haben gewonnen!");
+                                Console.WriteLine("Gewinn: " + bet * 36);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Sie haben verloren!");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ungültige Wette, bitte erneut versuchen");
+                        }
+                        if (timer.ElapsedMilliseconds / 1000 >= timeLimit)
+                        {
+                            Console.WriteLine("Zeitlimit überschritten, Sie haben verloren.");
+                            continue;
                         }
 
-                } while (true);
-                
+
+
+
+                    } while (timer.ElapsedMilliseconds / 1000 < timeLimit);
+
+
+                }
             } 
 
         }while (true);
